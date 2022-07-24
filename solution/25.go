161bd3853bdb4b34bfe75adfbd01714997b47f38
yura <yura11011@yandex.com>
2022-07-24 20:26:55 +0300

    exercise 25

---
+++ solution/25.go
@@ -0,0 +1,26 @@
+package solution
+
+import (
+	"fmt"
+	"time"
+)
+
+//Реализовать собственную функцию sleep.
+
+//спим указанное количество секунд
+func sleep(seconds int) {
+	timer := time.NewTimer(time.Duration(seconds) * time.Second)
+	//блокируемся, пока таймер не сработает и в канале не появится сообщение
+	_ = <-timer.C
+}
+
+func N25() {
+	//Отступ
+	fmt.Println()
+	//выводим что-то
+	fmt.Println("Hello, ")
+	//спим 5 с
+	sleep(5)
+	//выводим еще что-то
+	fmt.Println("World")
+}
