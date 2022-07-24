014b6e96042eefb43ed921c55794136cf45ec468
yura <yura11011@yandex.com>
2022-07-24 20:13:40 +0300

    simplified

--- solution/24.go
+++ solution/24.go
@@ -11,26 +11,30 @@ import (
 //которые представлены в виде структуры Point
 //с инкапсулированными параметрами x,y и конструктором.
 
-type Point interface {
-	Distance(anotherPoint Point) float64
-}
+//type Point interface {
+//	Distance(anotherPoint Point) float64
+//}
 
-type point struct {
+//type point struct {
+//	x int
+//	y int
+//}
+
+//x, y не видны за пределами пакета
+type Point struct {
 	x int
 	y int
 }
 
+//конструктор
 func NewPoint(x, y int) Point {
-	var newPoint Point = &point{x: x, y: y}
+	var newPoint Point = Point{x: x, y: y}
 	return newPoint
 }
 
-func (p *point) Distance(anotherPoint Point) float64 {
-	point, ok := anotherPoint.(*point)
-	if ok != true {
-		//не тот тип, расстояние не посчитать
-		return -1
-	}
+//метод нахождения расстояния
+//считаем по теореме Пифагора
+func (p *Point) Distance(point Point) float64 {
 	square1 := math.Pow(float64(p.x-point.x), 2.0)
 	square2 := math.Pow(float64(p.y-point.y), 2.0)
 	return math.Sqrt(square1 + square2)
@@ -43,10 +47,10 @@ func N24() {
 	x1, y1, x2, y2 := 1, 2, 4, 6
 	fmt.Println(x1, y1, x2, y2)
 	//создаем точки конструктором
-	//X1, X2 имеют тип Point (а не point)
+	//X1, X2 имеют тип Point
 	X1 := NewPoint(x1, y1)
 	X2 := NewPoint(x2, y2)
-	//считаем расстояние, вызывая метод интерфейса
+	//считаем расстояние, вызывая метод
 	//и выводим результат
 	D := X1.Distance(X2)
 	fmt.Println(D)
